588d9c9fd134fe4fff05dc0d93d253b14d0435d9
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-07-01 18:06:24 -0400

    added initial content from old portfolio

--- src/Routes/Contact/ContactPage.js
+++ src/Routes/Contact/ContactPage.js
@@ -3,8 +3,25 @@ import './ContactPage.css';
 
 function ContactPage() {
     return (
-        <main className="main-body">
-            <p>test contact</p>
+        <main classNameName="main-body">
+            <h5 id="contact">Contact Me:</h5>
+            <ul>
+                <li className="contact-info">hollymrogers@gmail.com</li>
+                <li className="contact-info">
+                    <a href="https://github.com/holly257/" target="_blank" tabindex="6">
+                        Github
+                    </a>
+                </li>
+                <li className="contact-info">
+                    <a
+                        href="https://www.linkedin.com/in/holly-rogers-1194a0178/"
+                        target="_blank"
+                        tabindex="7"
+                    >
+                        LinkedIn
+                    </a>
+                </li>
+            </ul>
         </main>
     );
 }
