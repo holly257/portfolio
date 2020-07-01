6832a96fdc824519b93bffdf62d41ca47ccec106
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-07-01 16:17:20 -0400

    revised for mobile view - temporary

--- src/Routes/About/AboutPage.js
+++ src/Routes/About/AboutPage.js
@@ -4,7 +4,7 @@ import './AboutPage.css';
 function AboutPage() {
     return (
         <main>
-            <div id="about-me">A B O U T &nbsp;&nbsp; M E</div>
+            {/* <div id="about-me">A B O U T &nbsp;&nbsp; M E</div>
             <div id="about-page">
                 <span className="split">
                     <h1 id="about-header">Hello!</h1>
@@ -29,6 +29,31 @@ function AboutPage() {
                 </span>
                 <span className="split">
                     <img id="about-img" src="/img/Holly1.jpg" alt="Hey! I am Holly."></img>
+                </span>
+            </div> */}
+            <div id="about-me">A B O U T &nbsp;&nbsp; M E</div>
+            <div id="about-page">
+                <span className="split">
+                    <h1 id="about-header">Hello!</h1>
+                    <h2 id="about-intro">My name is Holly Rogers.</h2>
+                    <img id="about-img" src="/img/Holly1.jpg" alt="Hey! I am Holly."></img>
+                    <h4 className="about">
+                        I am a full stack developer, currently based in the Atlanta area.{' '}
+                    </h4>
+                    <h4 className="about">
+                        I have always been interested in design, but I recently discovered full
+                        stack development and haven’t been able to stop since!
+                    </h4>
+                    <h4 className="about">
+                        I am looking for the opportunity to join a team of developers and
+                        collaborate on new projects.
+                    </h4>
+                    <h4 className="about">
+                        In my free time I enjoy traveling, reading, hiking, and lots of coffee.
+                    </h4>
+                    <h4 className="about">
+                        Feel free to reach out or browse some of my projects below!
+                    </h4>
                 </span>
             </div>
         </main>
