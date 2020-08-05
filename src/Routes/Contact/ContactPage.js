310e2c8cfe66bffaa64a8d2d697e05137b07168d
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-05 14:00:07 -0400

    layout and formatting for contact page

--- src/Routes/Contact/ContactPage.js
+++ src/Routes/Contact/ContactPage.js
@@ -1,33 +1,64 @@
 import React from 'react';
 import './ContactPage.css';
+import { FontAwesomeIcon } from '../../../node_modules/@fortawesome/react-fontawesome';
+import { faEnvelope, faFileAlt } from '../../../node_modules/@fortawesome/free-solid-svg-icons';
 
 function ContactPage() {
     return (
         <main className="main-body">
-            <h5 id="contact">Contact Me:</h5>
-            <ul>
-                <li className="contact-info">hollymrogers@gmail.com</li>
-                <li className="contact-info">
-                    <a
-                        href="https://github.com/holly257/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        tabIndex="6"
-                    >
-                        Github
-                    </a>
-                </li>
-                <li className="contact-info">
-                    <a
-                        href="https://www.linkedin.com/in/holly-rogers-1194a0178/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        tabIndex="7"
-                    >
-                        LinkedIn
-                    </a>
-                </li>
-            </ul>
+            <div>
+                <div>
+                    <h3 className="title-text">C O N T A C T</h3>
+                </div>
+                <div className="contact-container">
+                    <li className="contact-item contact-info">
+                        <FontAwesomeIcon icon={faEnvelope} className="contact-item" />
+                        <li className="contact-item">hollymrogers@gmail.com</li>
+                    </li>
+                    <br />
+                    <li className="contact-item">
+                        <img
+                            className="contact-logos contact-item"
+                            src="/img/GitHub-Mark-32px.png"
+                        ></img>
+                        <a
+                            href="https://github.com/holly257/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="contact-item"
+                            tabIndex="6"
+                        >
+                            Github
+                        </a>
+                    </li>
+                    <br />
+                    <li className="contact-item">
+                        <img className="contact-logos contact-item" src="/img/LI-In-Bug.png"></img>
+                        <a
+                            href="https://www.linkedin.com/in/holly-rogers-1194a0178/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="contact-item"
+                            tabIndex="7"
+                        >
+                            LinkedIn
+                        </a>
+                    </li>
+                    <br />
+                    <li className="contact-info contact-item">
+                        <FontAwesomeIcon className="contact-item" icon={faFileAlt} />
+                        <a
+                            href=""
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="contact-item"
+                            tabIndex="7"
+                        >
+                            &nbsp;Resume
+                        </a>
+                    </li>
+                </div>
+            </div>
         </main>
     );
 }
