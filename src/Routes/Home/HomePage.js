e739c6f3cf0632f5d901f1dae883e38e4b8c5437
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-06 15:45:32 -0400

    working on home page image

--- src/Routes/Home/HomePage.js
+++ src/Routes/Home/HomePage.js
@@ -7,6 +7,7 @@ function HomePage() {
             <div>
                 <h1 className="home-intro">H O L L Y &nbsp;&nbsp; R O G E R S</h1>
                 <h3 id="home-info">Full Stack Developer</h3>
+                <img id='profile-image' src='/img/IMG_1942.jpg'/>
             </div>
         </main>
     );
