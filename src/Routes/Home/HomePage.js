4bf9b8ba68d70944ce369a8d891906559d92123e
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-06 21:22:23 -0400

    small formatting on home

--- src/Routes/Home/HomePage.js
+++ src/Routes/Home/HomePage.js
@@ -5,9 +5,10 @@ function HomePage() {
     return (
         <main className="main-body" id="home">
             <div>
+                
+                <img id='profile-image' src='/img/IMG_1980_color.png'/>
                 <h1 className="home-intro">H O L L Y &nbsp;&nbsp; R O G E R S</h1>
                 <h3 id="home-info">Full Stack Developer</h3>
-                <img id='profile-image' src='/img/IMG_1942.jpg'/>
             </div>
         </main>
     );
