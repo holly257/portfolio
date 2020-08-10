9e5806e5fa55800caead5c5b86354ab3f142ab29
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-08-10 11:34:41 -0400

    switched home page image

--- src/Routes/About/AboutPage.js
+++ src/Routes/About/AboutPage.js
@@ -3,11 +3,11 @@ import './AboutPage.css';
 
 function AboutPage() {
     return (
-        <main className="main-body">
+        <main className="main-body" id="about-page-main">
             <div className="title-text">A B O U T &nbsp;&nbsp; M E</div>
             <div id="about-page">
                 <span className="split">
-                    <img id="about-img" src="/img/Holly1.jpg" alt="Hey! I am Holly."></img>
+                    <img id="about-img" src="/img/Portfolio_hero.png" alt="Builder, Designer, Leader, Collaborator"></img>
                     <h1 id="about-header">Hey,</h1>
                     <h2 id="about-intro">I'm Holly!</h2>
                     <h4 className="about">
--- src/Routes/Home/HomePage.js
+++ src/Routes/Home/HomePage.js
@@ -5,13 +5,15 @@ import { Link } from 'react-router-dom';
 function HomePage() {
     return (
         <main className="main-body" id="home">
-            <div className='split_home'>
-                <img id='profile-image' src=''/>
-            </div>
-            <div className='split_home'> 
-                {/* <h1 className="home-intro">W E L C O M E !</h1>
-                <h2 className="home-intro">Builder, Designer, Leader, Creative, Open-minded, Passionate, Team Driven</h2> */}
-                <Link id='more_link' to='/about'>Learn More</Link>
+            <div id="home-page">
+                <div className='split_home'>
+                    <img id="about-img" src="/img/Portfolio_hero.png" alt="Builder, Designer, Leader, Collaborator"></img>
+                </div>
+                <div className='split_home'> 
+                    {/* <h1 className="home-intro">W E L C O M E !</h1>
+                    <h2 className="home-intro">Builder, Designer, Leader, Creative, Open-minded, Passionate, Team Driven</h2> */}
+                    <Link id='more_link' to='/about'>Learn More</Link>
+                </div>
             </div>
         </main>
     );
